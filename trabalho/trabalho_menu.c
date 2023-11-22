80070714648d26d458a3429ed40e195bb0d15908
CampeloRoberto <142457717+CampeloRoberto@users.noreply.github.com>
2023-11-22 19:22:12 +0000

    trabalho versao 22/11/2023 16h21

--- trabalho/trabalho_menu.c
+++ trabalho/trabalho_menu.c
@@ -1,19 +1,20 @@
 /*   PENDENCIAS
 - ambições = menu/ score/ nao abrir o jogo na hora q da enter/ mudar dificuldade
 - selecionar dificuldade no menu (alterar tamanho da arena), mas como mudar se sao constantes ??
-- printf ("\e[?25l"); --- ?
+- strutc timeval, tv, STDIN_FILENO
 -canonical mode? disable echo? tcgetattr ?
-- /a ?
+- arrumar a contagem;
+-arrumar o print final;
 
 */
 
 #include <stdio.h>
 #include <stdlib.h> // para usar o system(), (clear), FD_SET, fd_set e o TDIN_FILENO
-#include <unistd.h> // para usar a  STDIN_FILENO e usleep
+#include <unistd.h> // para usar a  STDIN_FILENO e usleep/sleep
 #include <termios.h> // oldt, newt, tcgetattr, tcsetattr, ICANON, ECHO , TCANOW
 
 #define colunas 50 //esses valores determinam o tamanho da arena, reduzi-los aumentará a dificuldade.
-#define linhas 30
+#define linhas 20
 
 int main() {
   // PRIMEIRO PASSO: TIRAR O CURSOR ( se nao nao teremos como apertar os controles dos jogos)
@@ -55,7 +56,8 @@ char opcao  = 0;
 
             case '2':
             deu_certo = system("clear");
-            printf(" ---------BEM VINDO AO JOGO DA COBRINHA!!!----\n"); //adicionar modo dificil!! (ganha 3 pontos por coração)
+            printf(" ---------BEM VINDO AO JOGO DA COBRINHA!!!----\n\n"); //adicionar modo dificil!! (ganha 3 pontos por coração)
+            printf (" -----AUMENTE BASTANTE O TAMANHO DO CONSOLE!! -----");
             printf("\n");
             printf("\n");
             printf("    Para Mover a                             \n");
@@ -63,14 +65,42 @@ char opcao  = 0;
             printf("                       'W'                    \n");
             printf("                'A'    'S'   'D'              \n");
              printf("\n");
-            printf("  Para sair do jogo aperte a tecla      Esc   \n");
+            printf("  Para sair do jogo aperte a tecla      '0'   \n");
             printf("\n");
             printf("\n");
             printf("\n");
             printf("\n");
             printf("           APERTE ENTER PARA COMEÇAR!!!       \n"); //como fazer um 3... 2... 1... já!! trabalho demais?
+
             getchar();
             deu_certo = system("clear");
+           /* printf("\n");
+            printf("\n");
+            printf("\n");
+
+            printf("                        3                         "); // ----------------------- ta bugando..nao sei o que fazer
+            sleep (1);
+            deu_certo = system("clear");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("                        2                         ");
+            sleep (1);
+            deu_certo = system("clear");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("\n");
+            printf("                        1                         ");
+            sleep (1);
+            deu_certo = system("clear"); // ta bugado, fica aparecendo de novo a ultima coisa que eu fiz :///
+
+            sleep (1);*/
+
+
 
 
 
@@ -83,6 +113,7 @@ char opcao  = 0;
   newt = oldt;
   newt.c_lflag &= ~(ICANON | ECHO);
   tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+
 
 // definindo a área do jogo, se não a cobra some
   int x[1000], y[1000];
@@ -100,6 +131,7 @@ char opcao  = 0;
       printf("│\n"); // linhas verticais da borda direita
     }
 
+  printf("\e[?25l");// ---------------------------------------------tirando o cursor de novo pq reapareceu sozinho
     printf("└"); // quina inferior esquerda
     for (int i = 0; i < colunas; i++)
       printf("─"); // linhas horizontais da base
@@ -109,57 +141,64 @@ char opcao  = 0;
     printf("\e[%iA", linhas + 2);
 
     int cabeca = 0, cauda = 0; // declarando variaveis da cobra;
-    x[cabeca] = colunas / 2; // vetor de posição  da cobra horizontal
+    x[cabeca] = colunas / 2; // vetor de posição  da cobra horizontal, dividio por 2 para a referencia ser o meio
     y[cabeca] = linhas / 2; // vetor de posição da cobra vertical
-    int game_over = 0;
-    int xdir = 1, ydir = 0;
-    int applex = -1, appley;
-    int score = 0;
+    int game_over = 0; //deve começar em zero, ja que em 1 o jogo acaba
+    int xdir = 1, ydir = 0; // variaveis de direção, o xdir esta em 1 para a cobrinha começar a ir para a direita automaticamente
+    int comidax = -1; /*tem que ser negatico para aparecer uma comida, a condição if só cria a
+    comida caso o valor atribuido a comida seja negativo;*/
+    int comiday; // nao precisa de valor inicial
+    int score = 0; // boas praticas de programação, é o marcador de pontos
 
-    while (!quit && !game_over) {
-      if (applex < 0) {
-        // definindo posição aleatória da comida
-        applex = rand() % colunas;
-        appley = rand() % linhas;
+    while (!quit && !game_over) { //enquanto a pessoa nao perde ou nao aperta o botao de sair...
+      if (comidax < 0) {
+        // definindo posição aleatória da comida no eixo x e no eixo y;
+        comidax = rand() % colunas;
+        comiday = rand() % linhas;
 
-        for (int i = cauda; i != cabeca; i = (i + 1) % 1000)
-          if (x[i] == applex && y[i] == appley)
-            applex = -1;
-
-        if (applex >= 0) {
+        if (comidax >= 0) {
           // printando a comida
-          printf("\e[%iB\e[%iC\e[0;31m▓\e[0m", appley + 1, applex + 1); //mudando para vermelho e o simbolo para quadrado!!
-          printf("\e[%iF", appley + 1);
+          printf("\e[%iB\e[%iC\e[0;31m▓\e[0m", comiday + 1 , comidax + 1); //mudando para vermelho e o simbolo para quadrado!! +1 pra n aparecer comida na borda
+          printf("\e[%iF", comiday + 1); /*\e[%iB  e \e[%iC são prints posicionais, sendo C posição coluna, B posição da linha e
+           \e[%iF retira as informações do console nao sai o local do arquivo no final*/
           score = score + 1; // aqui definimos quantas comidas a cobra comeu!   ❤
         }
       }
 
-      // limpando a cauda da cobra
+      // limpando a cauda da cobra, sem isso vai ficando o rastro
       printf("\e[%iB\e[%iC ", y[cauda] + 1, x[cauda] + 1);
       printf("\e[%iF", y[cauda] + 1);
 
-      if (x[cabeca] == applex && y[cabeca] == appley) {
-        applex = -1;
-        printf("\a"); // Bell
-      } else
-        cauda = (cauda + 1) % 1000;
+      if (x[cabeca] == comidax && y[cabeca] == comiday) { // aqui e quanto a cobra come a comida.
+        comidax = -1; // -1, a comida some
 
-      int nova_cabeca = (cabeca + 1) % 1000;
+      } else{
+          cauda = (cauda + 1) % 1000; /*esse numero tem que ser alto, cada vez que a cauda amenta o valor deve se manter baixo,
+           se nao a cauda nao limpa*/
+      }
+
+
+      int nova_cabeca = (cabeca + 1) % 1000; //na verdade a cabeça da cobra sempre vai indo pra cauda, 0 +1 e so pra n dividir 0/1000
       x[nova_cabeca] = (x[cabeca] + xdir + colunas) % colunas;
       y[nova_cabeca] = (y[cabeca] + ydir + linhas) % linhas;
       cabeca = nova_cabeca;
 
-      for (int i = cauda; i != cabeca; i = (i + 1) % 1000)
+      for (int i = cauda; i != cabeca; i = (i + 1) % 1000){ //condições de derrota!, pode-se adicionar obstáculos ou colisao com parede
         if (x[i] == x[cabeca] && y[i] == y[cabeca])
+        {
           game_over = 1;
+        }
+
+      }
+
 
       // printando a cabeça da cobra
       printf("\e[%iB\e[%iC\e[0;32m▓\e[0m", y[cabeca] + 1, x[cabeca] + 1); // mudei para verde ▓
       printf("\e[%iF", y[cabeca] + 1);
-      fflush(stdout);
 
-      usleep(5 * 1000000 / 60); // aqui definimos a velocidade da cobra!! ela tem um formato de escrita, nao aceita so um valor
-
+      fflush(stdout); // ver o cursor frame a frame de acordo com a velocidade do usleep
+      /*aqui definimos a velocidade da cobra!! está em microssegundos, ou seja, 0,84 segundos - quanto menor mais rapida ela se move*/
+      usleep (84000);
 
       // para ler o teclado
       struct timeval tv;
@@ -172,18 +211,18 @@ char opcao  = 0;
       select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv); // definir os controles do programa; (posso colocar como variável em menu);
       if (FD_ISSET(STDIN_FILENO, &fds)) {
         int ch = getchar();
-        if (ch == 27 ) { // 27 = Esc
+        if (ch == '0' ) { // 27 = Esc
           quit = 1;
-        } else if (ch == 'a' && xdir != 1) {
+        } else if (ch == 'a'|| ch=='A' && xdir != 1) { // aqui definimos a tecla que vai mexer a cobrinha variando em x ou em y , mas esta invertido em x e y
           xdir = -1;
           ydir = 0;
-        } else if (ch == 'd' && xdir != -1) {
+        } else if (ch == 'd' || ch=='D' && xdir != -1) { //AQUI NAO TEMOS MAIS PROBLEMA COM CAPS LOCK
           xdir = 1;
           ydir = 0;
-        } else if (ch == 's' && ydir != -1) {
+        } else if (ch == 's' || ch=='S' && ydir != -1) {
           xdir = 0;
           ydir = 1;
-        } else if (ch == 'w' && ydir != 1) {
+        } else if (ch == 'w' || ch=='W' && ydir != 1) {
           xdir = 0;
           ydir = -1;
         }
@@ -193,15 +232,32 @@ char opcao  = 0;
     if (!quit) {
       // Show game over
       printf("\e[%iB\e[%iC Game Over!  ", linhas / 2, colunas/ 2 - 5);
-      printf("\e[%iB\e[%iC SCORE = %i ",linhas/2, colunas/2 - 6, score);
+      printf("\e[%iB\e[%iC SCORE = %i ",linhas/2 -20, colunas/2 -5 , score);
+      printf (" \e[%iB\e[%iC Para sair aperte '0' duas vezes\n", linhas/2 - 15, colunas- 200);
+      printf (" \e[%iB\e[%iC Para recomeçar tecle qualquer outra tecla", linhas/2 - 16, colunas-10); //esse trem n me obedece cara...---
+
       printf("\e[%iF", linhas / 2);
       fflush(stdout);
+      //estrutura que define os botoes da saida do jogo ou para recomeçar
+    int ch = getchar(); // nao queria que fosse de dois fatores
+       if ( ch == '0' ) { // 27 = Esc
+          quit = 1;
+          }else{
+             //getchar();
+      int deu_certo = system ("clear");
+
+      printf("\n");
+      printf("\n");
+      printf ("\n\n---------- Para recomeçar tecle qualquer tecla!! --------");
+
+          }
       getchar();
+      int deu_certo = system ("clear");
 
     }
   }
 
-  // REaparecer com o cursor
+  // Reaparecer com o cursor
   printf("\e[?25h");
 
   tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
